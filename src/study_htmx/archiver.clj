42ee0979b954512209abe546bd830e1bfb2744f3
Aditya Athalye <aditya.athalye@gmail.com>
2024-07-07 17:23:12 +0530

    WIP: move archiver code to its own ns for reuse

---
+++ src/study_htmx/archiver.clj
@@ -0,0 +1,73 @@
+(ns study-htmx.archiver
+  (:require
+   [clojure.data.json :as json]))
+
+(defn make-archiver
+  []
+  (agent {:status (atom :waiting)
+          :progress 0
+          :archive-file nil}
+         :validator (fn [a]
+                      (not (#{:cancelled :paused} @(:status a))))))
+
+(defonce archiver
+  (make-archiver))
+
+(defn do-archive!
+  [contacts total-contacts archiver]
+  (when (= @(:status @archiver) :waiting)
+    (let [file-path "resources/archive.json" #_(format "resources/%s.json" (random-uuid))]
+      (swap! (:status @archiver) (constantly :running))
+      (spit file-path "[")
+      (send archiver assoc
+            :archive-file file-path)))
+
+  (when (= @(:status @archiver) :running)
+    (doseq [contact contacts]
+      (send archiver update :progress inc)
+      (send-off archiver
+                (fn write-archive! [arch]
+                  (spit (:archive-file arch)
+                        (str (json/write-str contact)
+                             (if (= (:progress arch) total-contacts) "]" ","))
+                        :append true)
+                  (Thread/sleep 5000)
+                  arch))))
+  (send-off archiver (fn [a]
+                       (swap! (:status a) (constantly :done))
+                       a))
+  archiver)
+
+(defn pause-archive!
+  [archiver]
+  (swap! (:status @archiver) (constantly :paused))
+  archiver)
+
+(defn resume-archive!
+  [archiver]
+  (swap! (:status @archiver) (constantly :running))
+  (restart-agent archiver
+                 @archiver)
+  archiver)
+
+(defn reset-archive!
+  [archiver]
+  (alter-var-root #'archiver (constantly (make-archiver))))
+
+(defn cancel-archive!
+  [archiver]
+  (pause-archive! archiver)
+  (reset-archive! archiver))
+
+(comment
+  (require 'study-htmx.web-one-app)
+  (do-archive! (into [] @study-htmx.web-one-app/contacts-db)
+               (count @study-htmx.web-one-app/contacts-db)
+               archiver)
+
+  (pause-archive! archiver)
+  (resume-archive! archiver)
+  (reset-archive! archiver)
+  (cancel-archive! archiver)
+
+  (agent-error archiver))
--- src/study_htmx/web_one_app.clj
+++ src/study_htmx/web_one_app.clj
@@ -1,12 +1,11 @@
 (ns study-htmx.web-one-app
   (:require
    [clojure.string :as s]
-   [clojure.data.json :as json]
    [io.pedestal.interceptor :as interceptor]
    [study-htmx.response :as shr]
    [study-htmx.templates :as sht]
-   [hiccup2.core :as h2c]
-   [hiccup.page :as hp]))
+   [study-htmx.archiver :as archiver]
+   [hiccup2.core :as h2c]))
 
 (def ^{:private true} contacts-db-init
   (into {}
@@ -215,80 +214,11 @@
                (assoc context :response
                       (-> response shr/ok))))}))
 
-(defn make-archiver
-  []
-  (agent {:status (atom :waiting)
-          :progress 0
-          :archive-file nil}
-         :validator (fn [a]
-                      (not (#{:cancelled :paused} @(:status a))))))
-
-(defonce archiver
-  (make-archiver))
-
-(defn do-archive!
-  [contacts total-contacts archiver]
-  (when (= @(:status @archiver) :waiting)
-    (let [file-path "resources/archive.json" #_(format "resources/%s.json" (random-uuid))]
-      (swap! (:status @archiver) (constantly :running))
-      (spit file-path "[")
-      (send archiver assoc
-            :archive-file file-path)))
-
-  (when (= @(:status @archiver) :running)
-    (doseq [contact contacts]
-      (send archiver update :progress inc)
-      (send-off archiver
-                (fn write-archive! [arch]
-                  (spit (:archive-file arch)
-                        (str (json/write-str contact)
-                             (if (= (:progress arch) total-contacts) "]" ","))
-                        :append true)
-                  (Thread/sleep 5000)
-                  arch))))
-  (send-off archiver (fn [a]
-                       (swap! (:status a) (constantly :done))
-                       a))
-  archiver)
-
-(defn pause-archive!
-  [archiver]
-  (swap! (:status @archiver) (constantly :paused))
-  archiver)
-
-(defn resume-archive!
-  [archiver]
-  (swap! (:status @archiver) :running)
-  (restart-agent archiver
-                 @archiver)
-  archiver)
-
-(defn reset-archive!
-  [archiver]
-  (alter-var-root #'archiver (constantly (make-archiver))))
-
-(defn cancel-archive!
-  [archiver]
-  (pause-archive! archiver)
-  (reset-archive! archiver))
-
-(comment
-  (do-archive! (into [] @contacts-db)
-               (count @contacts-db)
-               archiver)
-
-  (pause-archive! archiver)
-  (resume-archive! archiver)
-  (reset-archive! archiver)
-  (cancel-archive! archiver)
-
-  (agent-error archiver))
-
 (def archive-of-contacts
   (interceptor/interceptor
    {:name ::archive-of-contacts
     :enter (fn [context]
-             (assoc context :archiver archiver))
+             (assoc context :archiver archiver/archiver))
     :leave (fn [context]
              (assoc context
                     :response
